339f4ce619d0a13b22c37fe81c23108e168440cc
plirapli <rafliyulistiawan@gmail.com>
2025-03-11 04:39:48 +0700

    fix: change db host

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("tcc-2", "root", "", {
-  host: "35.222.95.210",
+const db = new Sequelize("tcc-2", "root", "yourpassword", {
+  host: "146.148.81.251",
   dialect: "mysql",
 });
 
