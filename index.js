967e55f0a18ab140c1ede046c0897a362efac163
plirapli <rafliyulistiawan@gmail.com>
2025-03-11 01:20:21 +0700

    fix: root endpoint

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/", (req, res) => res.render("index"));
 app.use(UserRoute);
 
 app.listen(5000, () => console.log("Server connected"));
